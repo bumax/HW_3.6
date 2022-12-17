ee64aa4ab8660af95e7d8be3c532c7be57d26996
BuMax <max@butenko.in>
2022-12-17 23:02:55 +1000

    Exercise #41 completed

---
+++ ex41/Program.cs
@@ -0,0 +1,10 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите N чисел через запятую: ");
+var test = Console.ReadLine().Split(',').Select(int.Parse);
+
+var count = new int();
+// перебираем и считаем:
+foreach (var item in test) if(item > 0) count++;
+
+Console.WriteLine("Количество положительных чисел: {0}", count);
